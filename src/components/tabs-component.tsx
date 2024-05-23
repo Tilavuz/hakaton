452337d9249a919ff64238019e15fd3dd9eccea4
tilav <tilav@tilav.tilav>
2024-05-23 09:46:22 +0500

    changed datas.tsx

--- src/components/tabs-component.tsx
+++ src/components/tabs-component.tsx
@@ -1,4 +1,4 @@
-import { TargetIndicatorsFuture, columnFuture, columnNemis, rowsNemis } from '@/contexts/datas'
+import { targetIndicatorsFuture, columnPurposeful, columnNemis, rowsNemis, columnsDone, columnEnglish, rowsEnglish } from '@/contexts/datas'
 
 import Table from './table'
 import { TabsContent, Tabs, TabsList, TabsTrigger } from './ui/tabs'
@@ -13,13 +13,13 @@ export default function TabsComponent() {
             <TabsTrigger value="Nemis tiliga o'qiyotganlar ko'rsatkichlar">Nemis tiliga o'qiyotganlar ko'rsatkichlar</TabsTrigger>
         </TabsList>
         <TabsContent value="Maqsadli ko'rsatkichlar">
-          <Table columns={columnFuture} tableData={TargetIndicatorsFuture}/>
+          <Table columns={columnPurposeful} tableData={targetIndicatorsFuture}/>
         </TabsContent>
         <TabsContent value="Jami o'qiyotganlar ko'rsatkichlar">
-          <Table columns={columnFuture} tableData={TargetIndicatorsFuture}/>
+          <Table columns={columnsDone} tableData={targetIndicatorsFuture}/>
         </TabsContent>
         <TabsContent value="Ingliz tili o'qiyotganlar ko'rsatkichlar">
-          <Table columns={columnFuture} tableData={TargetIndicatorsFuture}/>
+          <Table columns={columnEnglish} tableData={rowsEnglish}/>
         </TabsContent>
         <TabsContent value="Nemis tiliga o'qiyotganlar ko'rsatkichlar">
           <Table columns={columnNemis} tableData={rowsNemis}/>
